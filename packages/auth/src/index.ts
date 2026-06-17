bfb2520cecf301da2ab876cffc31674d9d818098
wangleixxxy <wangleixxxy@163.com>
2026-06-18 00:53:34 +0800

    feat：init

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,33 @@
+import { createDb } from "@my-todo-list/db";
+import * as schema from "@my-todo-list/db/schema/auth";
+import { env } from "@my-todo-list/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export function createAuth() {
+  const db = createDb();
+
+  return betterAuth({
+    database: drizzleAdapter(db, {
+      provider: "pg",
+
+      schema: schema,
+    }),
+    trustedOrigins: [env.CORS_ORIGIN],
+    emailAndPassword: {
+      enabled: true,
+    },
+    secret: env.BETTER_AUTH_SECRET,
+    baseURL: env.BETTER_AUTH_URL,
+    advanced: {
+      defaultCookieAttributes: {
+        sameSite: "none",
+        secure: true,
+        httpOnly: true,
+      },
+    },
+    plugins: [],
+  });
+}
+
+export const auth = createAuth();
